2043bbfb01728eb8652999585563b78103842081
20euphoria <derek181295@gmail.com>
2021-12-19 17:08:44 +0530

    created 2 apps & post model

---
+++ PostApp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Post(models.Model):
+    user = models.models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=False)
