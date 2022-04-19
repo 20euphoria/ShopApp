bd4dcea43198fbabe2c4d60edbe812edc1f5fd89
Suraj Mahamuni <surajsm@maveric-systems.com>
2022-04-19 18:35:44 +0530

    removed the two apps & respected files

--- ShopApp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = [
-        "product_id", "name", "weight", "price", "created_at", "updated_at"
-        ]
